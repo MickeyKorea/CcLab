5dfb22bdd9de480cae682f808c46cc2d6e0401fe
MickeyKorea <yo637@nyu.edu>
2022-05-05 20:53:42 +0900

    project B update

--- Project_B/sketch.js
+++ Project_B/sketch.js
@@ -1,20 +1,37 @@
+let canvas;
+
 let client_id = '1d343529bfcf491cb4dff59b57d73eaa';
 let client_secret = '5e0b0d3e507f4e75bde95e3e1ea1f7d8';
 let testSong;
 let fft;
+
+let c1,c2;
 
 
 function preload(){
    testSong = loadSound('assets/song.mp3');
 }
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
   fft = new p5.FFT();
+  c1 = color(63,0,54);
+  c2 = color(11,0,32);
 }
 
 function draw() {
-  background(0);
+  for(let y=0; y<height; y++){
+    n = map(y,0,height,0,1);
+    let newColor = lerpColor(c1,c2,n);
+    stroke(newColor);
+    line(0,y,width,y);
+  }
   
   let waveform = fft.waveform();
   noFill();
